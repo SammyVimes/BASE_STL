d758462112df0fad7768f5c8e60482e1d4bb12db
Semen Danilov <samvimes@yandex.ru>
2013-10-21 01:52:05 +0400

    updated test

--- main.cpp
+++ main.cpp
@@ -10,14 +10,20 @@ int main() {
     Node<int>* left = root->getLeft();
     Node<int>* right = root->getRight();
     Node<int>* el = NULL;
-    left->setLeft(10);
+    left->setLeft(999);
     left->setRight(11);
     right->setLeft(5);
+    Node<int>* tmp = right->getLeft();
+    tmp->setLeft(100);
+    tmp->setRight(1248);
+    tmp = tmp->getRight();
+    tmp->setLeft(1000000);
+    tmp->setRight(999);
     el = left;
     left = el->getLeft();
     right = el->getRight();
     left->setLeft(19);
     right->setRight(21);
-    cout << tree->branchesToGo(19) << endl;
+    cout << tree->branchesToGo(999) << endl;
     return 0;
 }
